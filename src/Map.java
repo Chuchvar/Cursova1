48558213e8379a623b4337f85d46ac2ec52c8271
Chuchvar <127477368+Chuchvar@users.noreply.github.com>
2024-04-15 19:18:57 +0300

    map versin 1

--- src/Map.java
+++ src/Map.java
@@ -1,19 +1,27 @@
+import java.util.Arrays;
+
 public class Map {
     private int ger[][];
     private int ger2[][];
     private int get3[][];
-
+    private String map[][];
     public Map(int x,int y){
-<<<<<<< HEAD
-=======
-        int map[][]=new int[x][];
+        map=new String[x][];
         for (int r=0;r<x;r++){
-            map[r]=new int[y];
+            map[r]=new String[y];
+            System.out.println(Arrays.toString(map[r]));
         }
-        System.out.println(map);
->>>>>>> fd0157b0843d2cf555513cdfd737ad2dc7254679
     }
-
+    public Map(){
+        System.out.println("Ніхуя");
+    }
+    private void  spawn(int x,int y,String object) {
+        map[x-1][y-1]=object;
+        System.out.println(Arrays.deepToString(map));
+    }
+    public void getspan(int j,int t,String objectget){
+        spawn(j,t,objectget);
+    }
     public int[][] getGer() {
         return ger;
     }
--- src/World.java
+++ src/World.java
@@ -1,10 +1,12 @@
 import java.util.ArrayList;
 
 public class World {
-    Map tarra;
+
+    Map terra;
     int time;
     ArrayList fruits = new ArrayList<>();
     ArrayList fruits2 = new ArrayList<>();
+
 
     public World(int n,int x) { // конструктор, який генериє світ  в залежності від даної інформації
         for (int i=0;i<x;i++) {
@@ -14,7 +16,8 @@ public class World {
             fruits2.add(a2);
 
         }
-        this.tarra = new Map(n,n);
+        this.terra = new Map(n,n);
         this.time = 0;
+        terra.getspan(1,1,"Solider");
     }
 }
