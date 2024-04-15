8acf72fa4fffbc0b433e261022a28a87d4924160
moravsk2003 <kfcrfdjghjcbvj@gmail.com>
2024-04-15 14:31:47 +0300

    World

--- src/Map.java
+++ src/Map.java
@@ -1,3 +1,9 @@
 public class Map {
+    int ger[];
+    int ger2[];
+    int get3[];
 
+    public Map(int x,int y) {
+
+    }
 }
---
+++ src/World.java
@@ -0,0 +1,9 @@
+public class World {
+    Map tarra;
+    int time;
+
+    public World(int n) {
+        this.tarra = new Map(n,n);
+        this.time = time;
+    }
+}
