7651b4ef546a879db3368ffd68c6617b4735b42a
moravsk2003 <kfcrfdjghjcbvj@gmail.com>
2024-04-15 15:51:19 +0300

    World constructor

--- src/World.java
+++ src/World.java
@@ -4,12 +4,17 @@ public class World {
     Map tarra;
     int time;
     ArrayList fruits = new ArrayList<>();
+    ArrayList fruits2 = new ArrayList<>();
 
     public World(int n,int x) {
         for (int i=0;i<x;i++) {
             Soldier a = new Soldier();
             fruits.add(a);
-        }this.tarra = new Map(n,n);
+            Soldier a2 = new Soldier();
+            fruits.add(a2);
+
+        }
+        this.tarra = new Map(n,n);
         this.time = 0;
     }
 }
